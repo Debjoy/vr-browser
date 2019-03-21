13cf84283bb5240aea32da6ffcf87f5d466d76c9
Debjoy Bhowal <41815782+Debjoy@users.noreply.github.com>
2019-03-21 09:09:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+/* global AFRAME */
+
+/**
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
+ */
+var container="";
+AFRAME.registerComponent('set-image', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 0}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+
+    el.addEventListener(data.on, function () {
+      // Fade out image.
+      data.target.emit('set-image-fade');
+      // Wait for fade to complete.
+      setTimeout(function () {
+        // Set image.
+          if(data.src==="search"){
+              //document.getElementById("SearchArea").setAttribute('visible',"false");
+          }else if(data.src=="back"){
+              
+                container = container.substring(0, container.length-1);
+          }
+          else
+          container=container+data.src;
+          document.getElementById("searchbox").setAttribute('value',container);
+       // data.target.setAttribute('material', 'src', data.src);
+      }, data.dur);
+    });
+  }
+});
