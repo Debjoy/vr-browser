168c1006fcf5aec257e36dc1dc4f7336fd321fb6
Debjoy Bhowal <41815782+Debjoy@users.noreply.github.com>
2019-03-26 22:29:56 +0530

    Update script.js
    added Search functionality

--- script.js
+++ script.js
@@ -25,13 +25,12 @@ AFRAME.registerComponent('set-image', {
       setTimeout(function () {
         // Set image.
           if(data.src==="search"){
-              document.getElementById("SearchArea").setAttribute('visible',"false");
-            document.getElementById("searchresults").setAttribute('visible',"true");
+             search();
           }else if(data.src=="back"){
               
                 container = container.substring(0, container.length-1);
           }
-          else
+          else if(container.length<25)
           container=container+data.src;
           document.getElementById("searchbox").setAttribute('value',container);
        // data.target.setAttribute('material', 'src', data.src);
@@ -39,3 +38,59 @@ AFRAME.registerComponent('set-image', {
     });
   }
 });
+var path;
+
+function fetchbeforehand(){
+      var xhttp1 = new XMLHttpRequest();
+        
+          xhttp1.onloadend = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              path=this.responseText;
+              var startindex=path.indexOf("&");
+              var startindex2=path.indexOf("url+'")+5;
+              path=path.substr(startindex,path.indexOf("';",startindex)-startindex)+path.substr(startindex2,path.indexOf("';",startindex2)-startindex2);
+              
+            }else{
+                alert(this.responseText);
+            }
+          };
+          xhttp1.open("GET", "https://www.gigablast.com/search?q=hello", true);
+          xhttp1.send();
+}
+function search(){
+  
+   document.getElementById("SearchArea").setAttribute('visible',"false");
+    console.log(path);
+  
+        var xhttp = new XMLHttpRequest();
+        
+          xhttp.onloadend = function() {
+              //alert(this.responseText);
+            if (this.readyState == 4 && this.status == 200) {
+              showresult(this.responseText);
+              console.log(path);
+            }else{
+                alert(this.responseText);
+            }
+          };
+          xhttp.open("GET", "https://www.gigablast.com/search?q="+container+"&n=20"+path, true);
+          xhttp.send();
+}
+var el = document.createElement( 'html' );
+
+function showresult(msg){
+  el.innerHTML=msg;
+  var title= el.getElementsByClassName("title");
+  var desc=el.getElementsByClassName("title");
+  console.log("lel");
+  for(var i=0;i<20;i++){
+    var description=desc[i].parentElement.innerText.split("\n\n")[2];
+    description= description.replace(/(\r\n|\n|\r)/gm, "");
+    var ti=title[i].innerText;
+    document.getElementsByClassName("result_title")[i].setAttribute('value',ti.substr(0,25).trim()+"...");
+     document.getElementsByClassName("result_desc")[i].setAttribute('value',description.substr(0,150)+"...");
+  }
+  
+  document.getElementById("searchresults").setAttribute('visible',"true");
+  
+}
